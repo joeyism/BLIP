a3371ca4c91987e761c2a2d8cd83bd3e6cdc2afb
Joey <sham.joey@gmail.com>
2022-10-19 23:39:03 +0200

    Updated setup.py so it can install

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name="blip-model", 
     version="v1.0.0",
     packages=find_packages(exclude=["tests"]),
-    install_requires=[
-    ],
+    install_requires=[line.replace("\n", "") for line in open("requirements.txt", "r").readlines()],
     entry_points={
     },
     package_data={
