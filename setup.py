938b95381233a3f0711e2d528a40e4dc4a01f6c4
Joey <sham.joey@gmail.com>
2022-10-19 23:33:26 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="blip-model", 
+    version="v1.0.0",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+    ],
+    entry_points={
+    },
+    package_data={
+    },
+)
